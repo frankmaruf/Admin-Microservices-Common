1346f5178f900d422aa5279b834f93b8fc8b5bde
frankmaruf <thefrankmaruf@gmail.com>
2022-09-05 23:38:18 +0600

    First Commit

---
+++ src/InfluencerScope.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Microservices;
+
+use App\Services\UserService;
+use Closure;
+use Illuminate\Http\Request;
+
+class InfluencerScope
+{
+    private $userService;
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+    public function handle(Request $request, Closure $next)
+    {
+        if($this->userService->isInfluencer()) {
+            return $next($request);
+        }
+        return $next($request);
+    }
+}
